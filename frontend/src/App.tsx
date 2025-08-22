f67aaec5db3c0a4af49c34c7bcfd89f18ca4de26
Mandrindra <hei.mandrindra@gmail.com>
2025-08-22 12:53:26 +0200

    creat home page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 // App.tsx
 import TestColor from './components/TestColor';
-import Login from './components/forms/LoginForm';
+
+import HomePage from './pages/HomePage';
 
 const App: React.FC = () => {
   return (
-    <div className="grid w-[100%] h-screen place-items-center bg-cyan-400">
-      <Login />
+    <div>
+      <HomePage/>
     </div>
   );
 };

10bbe7c3c9be0b452fa4d06c35511bb94d17843f
david fils <miharisoadavidfils@gmail.com>
2025-09-05 12:18:10 +0300

    [ADD] add endpoint expense

--- backend/src/routes/expense.route.ts
+++ backend/src/routes/expense.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createExpense } from '../controllers/expense.controller';
+import { createExpense, getExpenseById, getExpenses, updateExpense, deleteExpense } from '../controllers/expense.controller';
 import { authenticateJWT } from '../middlewares/auth.middleware';
 import upload from '../middlewares/upload.middleware';
 
@@ -7,5 +7,10 @@ const router = Router();
 
 // Protéger la route et accepter un seul fichier nommé "receipt"
 router.post('/', authenticateJWT, upload.single('receipt'), createExpense);
+router.get('/', authenticateJWT, getExpenses);
+router.get('/:id', authenticateJWT, getExpenseById);
+router.put('/:id', authenticateJWT, updateExpense)
+router.delete('/:id', authenticateJWT, deleteExpense)
+
 
 export default router;

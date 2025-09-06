7e8fb624ded0902415f7d441146580e90a03b65b
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-06 23:56:19 +0300

    [add] finish to implement validation

--- backend/src/routes/summary.route.ts
+++ backend/src/routes/summary.route.ts
@@ -5,12 +5,14 @@ import {
   getCustomSummary,
   getMonthlySummary,
 } from "../controllers/summary.controller";
+import { validateQuery } from "../utils/validator";
+import { monthlyQuerySchema, summaryQuerySchema } from "../validator/summary.validator";
 
 const router = Router();
 router.use(authenticateJWT);
 
-router.get("/monthly", getMonthlySummary);
-router.get("/", getCustomSummary);
+router.get("/monthly", validateQuery(monthlyQuerySchema), getMonthlySummary);
+router.get("/", validateQuery(summaryQuerySchema) ,getCustomSummary);
 router.get("/alerts", getBudgetAlerts);
 
 export default router;

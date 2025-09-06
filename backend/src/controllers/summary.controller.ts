5817c132fbcc64158d91b12d1e599a9f31e58f94
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-06 11:10:28 +0300

    [add] finalize summary controlleur and endpoints

--- backend/src/controllers/summary.controller.ts
+++ backend/src/controllers/summary.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 
 export const getMonthlySummary = async (req: Request, res: Response) => {
   try {
+<<<<<<< HEAD
     const user = req.user;
     if (!user) {
       throw new Error("Unauthentificatied user");
@@ -30,6 +31,39 @@ export const getBudgetAlerts = async (req: Request, res: Response) => {
       console.trace(error.message);
       res.status(500).json({ message: error.message });
     }
+=======
+    const { month } = req.query;
+    const userId = req.user?.id;
+    if (!userId) {
+      res.status(403);
+      throw new Error("UnAuthoticated user");
+    }
+    const result = await SummaryService.getMonthlySummary(
+      String(userId),
+      String(month)
+    );
+    res.json(result);
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(500).json({ message: error.message });
+  }
+};
+export const getBudgetAlerts = async (req: Request, res: Response) => {
+  try {
+    const { month } = req.query;
+    const userId = req.user?.id;
+    if (!userId) {
+      res.status(403).json({ message: "UnAuthenticated User" });
+    }
+    const alerts = await SummaryService.getAlerts(
+      String(userId),
+      String(month)
+    );
+    res.json(alerts);
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(500).json({ message: error.message });
+>>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
   }
 };
 export const getCustomSummary = async (req: Request, res: Response) => {
@@ -49,12 +83,23 @@ export const getCustomSummary = async (req: Request, res: Response) => {
     if (!userId) {
       return res.status(403).json({ message: "UnAuthenticated User" });
     }
+<<<<<<< HEAD
     const customSummary = await SummaryService.getSummary(
+=======
+    const alerts = await SummaryService.getSummary(
+>>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
       userId,
       startDateParse,
       endDateParse
     );
+<<<<<<< HEAD
     res.json({ customSummary });
+=======
+    res.json({
+      periode: [startDateParse, endDateParse],
+      ...alerts,
+    });
+>>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
   } catch (error) {
     if (error instanceof Error)
       res.status(500).json({ message: error.message });

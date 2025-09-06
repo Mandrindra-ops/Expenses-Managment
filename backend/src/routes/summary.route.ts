14e7140d6af4d5293e963654ef7e2fc49a649198
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-06 09:57:26 +0300

    [add] create route for summary

--- backend/src/routes/summary.route.ts
+++ backend/src/routes/summary.route.ts
@@ -6,13 +6,16 @@ import {
   getMonthlySummary,
 } from "../controllers/summary.controller";
 import { validateQuery } from "../utils/validator";
-import { monthlyQuerySchema, summaryQuerySchema } from "../validator/summary.validator";
+import {
+  monthlyQuerySchema,
+  summaryQuerySchema,
+} from "../validator/summary.validator";
 
 const router = Router();
 router.use(authenticateJWT);
 
 router.get("/monthly", validateQuery(monthlyQuerySchema), getMonthlySummary);
-router.get("/", validateQuery(summaryQuerySchema) ,getCustomSummary);
+router.get("/", validateQuery(summaryQuerySchema), getCustomSummary);
 router.get("/alerts", getBudgetAlerts);
 
 export default router;

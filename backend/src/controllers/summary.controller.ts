41e2a86a6e1755eae502ef2b632f441778b1781e
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-06 15:08:07 +0300

    [fix] fix logic summary service

--- backend/src/controllers/summary.controller.ts
+++ backend/src/controllers/summary.controller.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 
 export const getMonthlySummary = async (req: Request, res: Response) => {
   try {
-<<<<<<< HEAD
     const user = req.user;
     if (!user) {
       throw new Error("Unauthentificatied user");
@@ -31,41 +30,22 @@ export const getBudgetAlerts = async (req: Request, res: Response) => {
       console.trace(error.message);
       res.status(500).json({ message: error.message });
     }
-=======
     const { month } = req.query;
     const userId = req.user?.id;
     if (!userId) {
       res.status(403);
       throw new Error("UnAuthoticated user");
     }
-    const result = await SummaryService.getMonthlySummary(
-      String(userId),
-      String(month)
-    );
-    res.json(result);
-  } catch (error) {
-    if (error instanceof Error)
-      res.status(500).json({ message: error.message });
-  }
-};
-export const getBudgetAlerts = async (req: Request, res: Response) => {
-  try {
-    const { month } = req.query;
-    const userId = req.user?.id;
-    if (!userId) {
-      res.status(403).json({ message: "UnAuthenticated User" });
+    const user = req.user;
+    if (!user) {
+      throw new Error("Unauthentificatied user");
     }
-    const alerts = await SummaryService.getAlerts(
-      String(userId),
-      String(month)
-    );
-    res.json(alerts);
-  } catch (error) {
-    if (error instanceof Error)
-      res.status(500).json({ message: error.message });
->>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
+    const summary = await SummaryService.getMonthlySummary(user.id.toString());
+
+    res.json(summary);
   }
 };
+
 export const getCustomSummary = async (req: Request, res: Response) => {
   try {
     const { start, end } = req.query;
@@ -83,23 +63,23 @@ export const getCustomSummary = async (req: Request, res: Response) => {
     if (!userId) {
       return res.status(403).json({ message: "UnAuthenticated User" });
     }
-<<<<<<< HEAD
-    const customSummary = await SummaryService.getSummary(
-=======
+
     const alerts = await SummaryService.getSummary(
->>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
       userId,
       startDateParse,
       endDateParse
     );
-<<<<<<< HEAD
+    const customSummary = await SummaryService.getSummary(
+      userId,
+      startDateParse,
+      endDateParse
+    );
     res.json({ customSummary });
-=======
     res.json({
       periode: [startDateParse, endDateParse],
       ...alerts,
     });
->>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
+    res.json({ customSummary });
   } catch (error) {
     if (error instanceof Error)
       res.status(500).json({ message: error.message });
--- backend/src/routes/summary.route.ts
+++ backend/src/routes/summary.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateJWT } from "../middlewares/auth.middleware";
-<<<<<<< HEAD
+
 import {
   getBudgetAlerts,
   getCustomSummary,
@@ -11,9 +11,6 @@ import {
   monthlyQuerySchema,
   summaryQuerySchema,
 } from "../validator/summary.validator";
-=======
-import { getBudgetAlerts, getCustomSummary, getMonthlySummary } from "../controllers/summary.controller";
->>>>>>> cecfb86 ([add] finalize summary controlleur and endpoints)
 
 const router = Router();
 router.use(authenticateJWT);

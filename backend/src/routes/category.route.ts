cf6b94bb5f0893543c2fa8b09d8a4127ef3a4380
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-08 15:48:22 +0300

    [fix] change validator on income

--- backend/src/routes/category.route.ts
+++ backend/src/routes/category.route.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Router } from "express";
 import { authenticateJWT } from "../middlewares/auth.middleware";
 import {
@@ -12,10 +13,19 @@ import {
   updateCategorySchema,
 } from "../validator/categories.validator";
 import { idParamSchema } from "../validator/base.validator";
+=======
+import { Router } from 'express';
+import { authenticateJWT } from '../middlewares/auth.middleware';
+import { listCategories, createCategory, updateCategory, deleteCategory } from '../controllers/category.controller';
+import { validateBody, validateQuery } from '../utils/validator';
+import {createCategorySchema,updateCategorySchema} from '../validator/categories.validator'
+import { idParamSchema } from '../validator/base.validator';
+>>>>>>> 86765c6 ([fix] change validator on income)
 const router = Router();
 
 router.use(authenticateJWT);
 
+<<<<<<< HEAD
 router.get("/", listCategories);
 router.post("/", validateQuery(createCategorySchema), createCategory);
 router.put(
@@ -25,5 +35,11 @@ router.put(
   updateCategory
 );
 router.delete("/:id", validateQuery(idParamSchema), deleteCategory);
+=======
+router.get('/', listCategories);
+router.post('/',validateBody(createCategorySchema), createCategory);
+router.put('/:id',validateQuery(idParamSchema),validateQuery(updateCategorySchema), updateCategory);
+router.delete('/:id',validateQuery(idParamSchema), deleteCategory);
+>>>>>>> 86765c6 ([fix] change validator on income)
 
 export default router;

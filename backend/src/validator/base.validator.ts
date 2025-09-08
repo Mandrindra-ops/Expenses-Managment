5462a6f9663f921b5025d3c8061ef06810382e16
david fils <miharisoadavidfils@gmail.com>
2025-09-08 16:19:22 +0300

    [UPDATE]: password schema and add validation for change-password route (more secure)

--- backend/src/validator/base.validator.ts
+++ backend/src/validator/base.validator.ts
@@ -5,5 +5,10 @@ export const idParamSchema = z.object({
     .transform((val) => Number(val)) )
 })
 export const emailSchema = z.string().email();
-export const passwordSchema = z.string().min(6, 'Password must have min 6 characters');
+export const passwordSchema = z.string()
+  .min(6, "Password must be at least 6 characters long")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Password must contain at least one digit")
+  .regex(/[@$!%*?&#]/, "Password must contain at least one special character");
 export const dateSchema = z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid Date Format (YYYY-MM-DD)');

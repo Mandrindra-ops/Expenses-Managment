d3988567084bb5bd9cf3dd65e160bd078663ad8f
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-08 17:37:45 +0300

    [fix] change validator income and expense

--- backend/src/validator/base.validator.ts
+++ backend/src/validator/base.validator.ts
@@ -1,7 +1,8 @@
 import z from "zod";
 
 export const idParamSchema = z.object({
-  id: z.number()
+  id: z.number().or(z.string().regex(/^\d+$/, "id doit être un entier")
+    .transform((val) => Number(val)) )
 })
 export const emailSchema = z.string().email();
 export const passwordSchema = z.string().min(6, 'Password must have min 6 characters');

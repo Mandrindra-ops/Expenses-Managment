ccbaad5450a195fe8e4a0e658b5b75cf77de8c8c
sedraValisoa <valioawalkersedra@gmail.com>
2025-09-11 00:02:49 +0300

    [ref] add logout and redirect in home

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Navbar from '../components/forms/Navbar';
 import HeroSection from '../components/forms/HeroSection';
 import Footer from '../components/forms/Footer';
+import { useAccountStore } from '../store';
+import { Navigate } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
+    const authStatus = useAccountStore(state => state.status)
+
+    if (authStatus == "pending") return <div>Loading...</div>
+    if (authStatus == "authorized") return <Navigate to="/dashboard" />
+
     return (
         <div className="min-h-screen flex flex-col bg-neutral-50">
 

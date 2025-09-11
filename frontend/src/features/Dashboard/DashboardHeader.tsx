eeabfd2d4dad286d68138fb3aea2267c9f0f9fe1
Mandrindra <hei.mandrindra@gmail.com>
2025-09-11 10:45:50 +0200

    Remove user image from dashboard navbar

--- frontend/src/features/Dashboard/DashboardHeader.tsx
+++ frontend/src/features/Dashboard/DashboardHeader.tsx
@@ -41,22 +41,12 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({setSidebarOpen}) => {
                     {darkMode ? "Light Mode" : "Dark Mode"}
                 </button>
 
-                <button className="flex text-[var(--color-text-sub)] items-center focus:outline-none">
+                <button className="pr-5 flex text-[var(--color-text-sub)] items-center focus:outline-none">
                     {/* cloche de notif */}
                     <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                 </button>
-            
-                <div className="relative">
-                    <button className="relative flex items-center focus:outline-none">
-                        <img
-                            className="w-10 h-10 rounded-full border-2 border-[var(--color-primary)]"
-                            src="https://via.placeholder.com/40"
-                            alt="User"
-                        />
-                    </button>
-                </div>
             </div>
         </header>
     );

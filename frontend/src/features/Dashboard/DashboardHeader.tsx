20ac6f2c31f2b3adb07dde8319e50453e2cd7fb0
PiTCHuu <hei.amboara.2@gmail.com>
2025-09-11 15:11:03 +0200

    feat: cahnging the theme (#58)
    Co-authored-by: Amboara <amboaraniainarkt@gmail.com>

--- frontend/src/features/Dashboard/DashboardHeader.tsx
+++ frontend/src/features/Dashboard/DashboardHeader.tsx
@@ -21,7 +21,7 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({ setSidebarOpen, usern
 
     return (
         <header className="flex items-center justify-between h-18 bg-[var(--color-bg-card)] border-b border-gray-200">
-            <h2 className="text-2xl text-emerald-400 font-bold">{JSON.stringify(username) ?? "User"}</h2>
+
             <div className="flex items-center">
                 <button
                     className="text-[var(--color-text-sub)] hover:text-[var(--color-text)] lg:hidden"
@@ -34,24 +34,32 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({ setSidebarOpen, usern
             </div>
 
             <div className="flex items-center gap-4">
-                <button
+                <div 
                     onClick={() => setDarkMode(!darkMode)}
-                    className="
-                        px-4 py-2 rounded-lg shadow-md
-                        bg-[var(--color-primary)] text-white
-                        hover:brightness-90 transition text-sm
-                    "
-                >
-                    {darkMode ? "Light Mode" : "Dark Mode"}
-                </button>
+                    className="w-16 h-8 flex items-center bg-[var(--color-bg-card)] rounded-full p-1 cursor-pointer border border-gray-300 shadow-md transition"
+                    >
+                    <div
+                        className={`w-6 h-6 rounded-full shadow-md transform transition-all duration-300 
+                        ${darkMode ? "translate-x-8 bg-[var(--color-secondary)]" : "translate-x-0 bg-[var(--color-primary)]"}`}
+                    />
+                    </div>
+                    <p className="ml-3 text-sm text-[var(--color-text)]">
+                    {darkMode ? "Dark Mode" : "Light Mode"}
+                    </p>
 
-                <button className="pr-5 flex text-[var(--color-text-sub)] items-center focus:outline-none">
+                <button className=" flex text-[var(--color-text-sub)] items-center focus:outline-none">
                     {/* cloche de notif */}
                     <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 </button>
+                <div className="mr-5 flex items-center border rounded-3xl border-gray-300">
+                    <div className="w-10 h-10 rounded-full bg-[var(--color-primary)] flex items-center justify-center text-white font-bold">
+                        {username ? username.charAt(0).toUpperCase() : "U"}
+                    </div>
+                </div>
             </div>
+            
         </header>
     );
 };
